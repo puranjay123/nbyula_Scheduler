17ecc3cedc34e86d2bb19c67b2bb5dead2e90c37
puranjay123 <kwattrapuranjay@gmail.com>
2022-09-19 17:03:45 +0530

    added 16 modules

--- vastumantra/App/views.py
+++ vastumantra/App/views.py
@@ -11,7 +11,7 @@ import datetime
 
 
 
-client = FaunaClient(secret="")
+client = FaunaClient(secret="fnAEwyz9miACS-JIP4ljPrO0D506SLYVf_iIOrVW")
 indexes = client.query(q.paginate(q.indexes()))
 
 
--- vastumantra/vastumantra/urls.py
+++ vastumantra/vastumantra/urls.py
@@ -1,7 +1,6 @@
-"""vastumantra URL Configuration
-
+"""AppointmentScheduler URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +13,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  
-from django.conf import settings  
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns  
-from django.contrib.staticfiles.urls import static  
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
-urlpatterns = [  
- path('admin/', admin.site.urls),  
- path('', include("App.urls")),  
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("App.urls")),
 
-]  
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
